0869a7fbda486044f5ce1e6552008429d2ac6b39
Anderson Carvalho <andersonlimacrv@gmail.com>
2023-04-15 20:20:33 -0300

    Adicionado responsividade correta, começo de parte lógica login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        return render(request, 'cadastro.html')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            return redirect('/usuarios/cadastro')
+        
+        return HttpResponse('Teste')
